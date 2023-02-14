30e73564e0e3c24f3815b50c894e264230a61dad
yelynnpaing <95997370+yelynnpaing@users.noreply.github.com>
2023-02-14 23:17:31 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function App() { 
+  const [firstName , setFirstName] = useState('');
+  const [lastName , setLastName] = useState('');
+  const fullName = firstName + ' ' + lastName ;
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value)
+  }
+  const handleSecondNameChange = (e) => {
+    setLastName(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h5>Let’s check you in</h5>
+      <label>
+        First Name : {''}
+        <input 
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Second Name : {''}
+        <input 
+          value={lastName}
+          onChange={handleSecondNameChange}
+        />
+      </label>
+      <div>
+        Full Name : {fullName}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
